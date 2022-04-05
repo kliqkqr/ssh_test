a25fe16926e9a74a8d43718ef95d1bf279e51a3d
kliqkqr <paul.prietzel@gmail.com>
2022-04-06 01:39:20 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-    std::cout << "Hello World" << std::endl;
+    int rank, size;
+
+    MPI_Init(&argc, &argc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    std::cout << "Rank: " << rank << "; Size: " << size << ";" << std::endl;
     return 0;
 }
